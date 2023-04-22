d5cc3d0c45c1a72f6d0b22e07a292780348f0f29
lnuic <lnuic123@gmail.com>
2023-04-22 10:23:45 +0200

    init commit

---
+++ src/test/java/com/example/photoService/PhotoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photoService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
